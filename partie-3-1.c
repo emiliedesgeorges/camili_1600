3cee3688668659bc1f40dc28ca2f49f6ec82dcb1
cam02men <112418860+cam02men@users.noreply.github.com>
2023-02-18 15:22:00 -0500

    Create partie-3-1.c
    TP1 exercice 3.1

---
+++ partie-3-1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+    int val1, val2, a;
+    printf("Entrez le premier matricule: \n");
+    scanf("%d", &val1);
+    printf("Entrez le seconde matricule: \n");
+    scanf("%d", &val2);
+
+    a = ((val1 + val2) % 13) + 3;
+
+    if((a % 3 == 0) && (a != 3) && (a % 2 != 0)){
+        printf("le chiffre est impair, divisible par 3, mais n'est pas 3 ");
+    }
+    printf("le chiffre n'est pas impair et divisible par 3");
+    return 0;
+}
