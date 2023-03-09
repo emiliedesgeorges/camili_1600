38ce7c7074e05be02feef47c203f1637d6bce01b
cam02men <112418860+cam02men@users.noreply.github.com>
2023-03-09 17:56:13 -0500

    tout est beau :)

--- TP2/Sources TP2/chaine_de_caractere/anagramme/anagramme.s
+++ TP2/Sources TP2/chaine_de_caractere/anagramme/anagramme.s
@@ -4,7 +4,7 @@ string1:
 lenght1:
 .int 5
 string2:
-.asciz  "niche"
+.asciz  "niche" 
 lenght2:
 .int 5
 
@@ -13,9 +13,65 @@ lenght2:
 main:
 anagramme: 
 
-##VOTRE CODE ICI
+## comparer la longueur des mots
+mov lenght1, %ebx
+mov lenght2, %ecx
+cmp %ebx, %ecx
+jne longueurDifferentes
 
+mask:
+xor %eax, %eax          
+xor %ecx, %ecx
+xor %ebx, %ebx
+xor %edx, %edx
 
+## La somme des lettres divisés par la longueur du mot 
+movl $string1, %esi 
 
+n_c:
+lodsb
+cmp $0x00, %al ##regarder si on est a la fin du mot
+je division1
+addl %eax, %ecx
+jmp n_c
+
+division1:
+movl %ecx, %eax
+movl lenght1, %ecx
+div %ecx
+movl %eax, %ecx
+
+movl $string2, %esi
+
+n_c2:
+lodsb
+cmp $0x00, %al ##regarder si on est a la fin du mot
+je division2
+addl %eax, %ebx
+jmp n_c2
+
+division2:
+xor %edx, %edx
+movl %ebx, %eax
+movl lenght2, %ebx
+div %ebx
+movl %eax, %ebx
+
+cmp %ebx, %ecx
+je estAnagramme
+jmp pasAnagramme
+
+longueurDifferentes:
+movl $1, %eax
+jmp resultat
+
+estAnagramme:
+movl $0, %eax
+jmp resultat
+
+pasAnagramme:
+movl $255, %eax
+
+resultat:
 ret
-push %eax
+push %eax 
